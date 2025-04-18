3df328363f8ebd3d2de1728795debc505f6b850c
munircudi <38553707+munircudi@users.noreply.github.com>
2025-04-18 17:58:50 +0000

    test

--- news/settings.py
+++ news/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 STATIC_URL = '/static/'
 
